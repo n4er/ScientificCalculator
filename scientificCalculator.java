54a528d94b3e673aa99df01d86f0f8b87a0ef462
Nasser Almusalhi <nasser432243@gmail.com>
2025-07-20 15:58:04 +0400

    Creating the classes basic arithmetic methods

--- scientificCalculator.java
+++ scientificCalculator.java
@@ -11,4 +11,32 @@ public class scientificCalculator {
         System.out.println("6. Power");
         System.out.println("0. Exit");
     }
+    public static double add(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2){
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2){
+        if (num2 == 0){
+            throw new ArithmeticException("Cannot divide by zero!");
+        }
+        return num1 / num2;
+    }
+
+    public static double squareRoot(double num){
+        if (num < 0){
+            throw new IllegalArgumentException("Cannot calculate the square root of a negative number");
+        }
+        return Math.sqrt(num);
+    }
+    public static double calculatePower(double base, double exponent){
+        return Math.pow(base, exponent);
+    }
 }
