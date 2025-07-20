a3e8e426fc4cd80913896aa008e0f549de4551d4
Nasser Almusalhi <nasser432243@gmail.com>
2025-07-20 15:54:30 +0400

    Creating the display menu class

--- scientificCalculator.java
+++ scientificCalculator.java
@@ -1,5 +1,14 @@
 import java.util.Scanner; //used read input from the user
 
 public class scientificCalculator {
-    
+    public static void displayMenu(){
+        System.out.println("--- Scientific Calculator ---");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Square Root");
+        System.out.println("6. Power");
+        System.out.println("0. Exit");
+    }
 }
